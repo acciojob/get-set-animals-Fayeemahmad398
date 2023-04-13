736846f218d1b3372d3c149df6ab1a9082e3592d
Fayeemahmad398 <117560978+Fayeemahmad398@users.noreply.github.com>
2023-04-13 14:28:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species){
+		return this.species;
+	}
+	makeSound() {
+		
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	super();
+bark(){
+	console.log("woof");
+}
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	super();
+	purr(){
+	console.log("purr");
+}
+	
+	
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
